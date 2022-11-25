e0d1ec3ea63df98287184007f19e8db5c164bf28
Mohamed Yasser <mohamed1902824@miuegypt.edu.eg>
2022-11-25 16:45:20 +0200

    firebase imported

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:breakergame/routes.dart';
+import 'package:firebase_core/firebase_core.dart' as firebase_auth;
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
