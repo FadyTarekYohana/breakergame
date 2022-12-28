ec2778222426638f32a96d899669a0463dfc6650
Mohamed Yasser <mohamed1902824@miuegypt.edu.eg>
2022-12-29 00:07:42 +0200

    start for api

---
+++ lib/models/post.dart
@@ -0,0 +1,31 @@
+// To parse this JSON data, do
+//
+//     final post = postFromJson(jsonString);
+
+import 'dart:convert';
+
+List<Post> postFromJson(String str) =>
+    List<Post>.from(json.decode(str).map((x) => Post.fromJson(x)));
+
+String postToJson(List<Post> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Post {
+  Post({
+    required this.text,
+    required this.author,
+  });
+
+  String text;
+  String author;
+
+  factory Post.fromJson(Map<String, dynamic> json) => Post(
+        text: json["text"],
+        author: json["author"] == null ? null : json["author"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "text": text,
+        "author": author == null ? null : author,
+      };
+}
