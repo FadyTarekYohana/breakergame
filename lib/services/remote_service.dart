1770534cddb218bd49f11021a7901500357a2599
Mohamed Yasser <mohamed1902824@miuegypt.edu.eg>
2022-12-29 20:31:57 +0200

    api

---
+++ lib/services/remote_service.dart
@@ -0,0 +1,14 @@
+import 'package:breakergame/models/post.dart';
+import 'package:http/http.dart' as http;
+
+class RemoteService {
+  Future<List<Post>?> getPosts() async {
+    var client = http.Client();
+    var uri = Uri.parse('https://type.fit/api/quotes');
+    var response = await client.get(uri);
+    if (response.statusCode == 200) {
+      var json = response.body;
+      return postFromJson(json);
+    }
+  }
+}
