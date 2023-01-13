d2ea86057d3f8325f281faefdc8eb3b2613a1b5c
Mohamed Yasser <mohamed1902824@miuegypt.edu.eg>
2023-01-13 19:50:29 +0200

    authorization

---
+++ lib/routing/auth.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+import '../presentation/home_screen.dart';
+import '../presentation/login_screen.dart';
+
+class Authorize extends StatelessWidget {
+  const Authorize({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const HomePage();
+          } else {
+            return const Login();
+          }
+        },
+      ),
+    );
+  }
+}
