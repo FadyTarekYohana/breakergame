c86c8471536bbff8b6791cec7173d7a195636ca7
Hakoish01 <ahmedhamouda.1672001@gmail.com>
2023-01-22 11:24:12 +0200

    edits

--- lib/routing/routes.dart
+++ lib/routing/routes.dart
@@ -16,48 +16,58 @@ import '../presentation/signup_screen.dart';
 final GoRouter router = GoRouter(
   routes: [
     GoRoute(
+      //to check for authentication
       path: '/',
       builder: (BuildContext context, GoRouterState state) => const Authorize(),
     ),
     GoRoute(
+      //to go to signup page
       path: '/signup',
       builder: (BuildContext context, GoRouterState state) => const SignUp(),
     ),
     GoRoute(
+      //to go to home page
       path: '/homepage',
       builder: (BuildContext context, GoRouterState state) => const HomePage(),
     ),
     GoRoute(
+      //to go to game screen
       path: '/gamescreen',
       builder: (BuildContext context, GoRouterState state) =>
           GameScreen(level: state.extra.toString()),
     ),
     GoRoute(
+      //to go to credit screen
       path: '/creditsscreen',
       builder: (BuildContext context, GoRouterState state) =>
           const CreditsScreen(),
     ),
     GoRoute(
+      // to go to profile screen
       path: '/profilescreen',
       builder: (BuildContext context, GoRouterState state) =>
           const ProfileScreen(),
     ),
     GoRoute(
+      //to go to options screen
       path: '/optionsscreen',
       builder: (BuildContext context, GoRouterState state) =>
           const OptionsScreen(),
     ),
     GoRoute(
+      //to go to shop screen
       path: '/shopscreen',
       builder: (BuildContext context, GoRouterState state) =>
           const ShopScreen(),
     ),
     GoRoute(
+      //to go to level screens
       path: '/levels',
       builder: (BuildContext context, GoRouterState state) =>
           const LevelsScreen(),
     ),
     GoRoute(
+      //to go to level builder as admin
       path: '/levelbuilder',
       builder: (BuildContext context, GoRouterState state) =>
           const LevelBuilder(),
